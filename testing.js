de02a7b3124ff97234fd7e57638efb62963c3688
Jared Thomas <jjthom87@yahoo.com>
2016-07-30 12:19:54 -0400

    final changes for first page

--- testing.js
+++ testing.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table");
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -27,36 +26,20 @@ var quantityQuestion = [{
 	name: "quantityPick"
 }];
 
-var table = new Table({ head: ['Product ID', 'Product', 'Department', 'Price', 'Qty in Stock']});
 
-function displayData(){
-	connection.query('SELECT MAX(id) FROM Products', function(err, resultz) {
-		resultz.forEach(function(columns){
-			var chosen = columns['MAX(id)'];
-			connection.query('SELECT * FROM Products', function(err, results){
-				if (err) {
-					throw err;
-				}
-				for(i=0; i < results.length; i++) {
-					table.push(
-						[results[i].id, results[i].ProductName, results[i].DepartmentName, results[i].Price, results[i].StockQuantity]
-					);
-					if (table.length == chosen) {
-						console.log(table.toString());
-					}
-				};
-				searchAndBuy();
-			})
-		})
-	})
-};
-displayData();
-
-function searchAndBuy(){
+		function searchAndBuy(){
 	connection.query('SELECT * FROM Products', function(err, results){
 		if (err) {
 			throw err;
 		}
+		// results.forEach(function(column){
+		// 	console.log("-------------------------");
+		// 	console.log("Item ID: " + column.id);
+		// 	console.log("Item: " + column.ProductName);
+		// 	console.log("Price: $" + column.Price);
+		// 	console.log(column.StockQuantity + " in Stock");
+		// 	connection.end();
+		// };
 		inquirer.prompt(idQuestion).then(function(answer){
 			for(var i=0; i < results.length; i++) {
 				if(results[i].id == answer.idPick) {
@@ -65,23 +48,25 @@ function searchAndBuy(){
 						if (chosenItem.StockQuantity > answer2.quantityPick) {
 							var sale = (answer2.quantityPick * chosenItem.Price);
 							console.log("We have enough in stock. Your order has been placed");
-							connection.query("UPDATE Products SET ? WHERE ?",[{
+							connection.query("UPDATE Products, Departments SET ? WHERE ?",[{
 							StockQuantity: chosenItem.StockQuantity - answer2.quantityPick }, {id: answer.idPick}], function(err,res){
 								if (err){
 									throw err;
 									}
-							console.log("The Cost of Your Purchase for " + answer2.quantityPick + " " + chosenItem.ProductName + " is $" + sale + ".")
-							console.log("Thank You for Shopping.")
-							connection.end();
+								console.log("The Cost of Your Purchase for " + answer2.quantityPick + " " + chosenItem.ProductName + " is $" + sale + ".")
+								console.log("Thank You for Shopping.")
+								connection.end();
 								});
-						} else {
+							} else {
 							console.log("Not enough in stock. Order can not be placed.");
 							console.log("Please check again soon.")
-							connection.end();
-						}
-					})
+							connection.end()
+							}
+						})
+					}
 				}
-			}
+			})
 		})
-	})
 };
+
+searchAndBuy();
